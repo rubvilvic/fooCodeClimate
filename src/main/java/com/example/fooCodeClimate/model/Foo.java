e8261b4c18cb6d42addf64023c46fe4768302ff3
Rubén <ruben.villagarcia@gmail.com>
2018-04-17 10:35:44 +0200

    Add random bean

---
+++ src/main/java/com/example/fooCodeClimate/model/Foo.java
@@ -0,0 +1,94 @@
+package com.example.fooCodeClimate.model;
+
+public class Foo {
+
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private Integer age;
+
+    private String comments;
+
+    private String role;
+
+    private String foo;
+
+    private String bar;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFoo() {
+        return foo;
+    }
+
+    public void setFoo(String foo) {
+        this.foo = foo;
+    }
+
+    public String getBar() {
+        return bar;
+    }
+
+    public void setBar(String bar) {
+        this.bar = bar;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
