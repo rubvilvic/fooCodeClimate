71032349f2c8970ddff9f39fbf82118bd85c1108
Rubén <ruben.villagarcia@gmail.com>
2018-04-17 10:32:21 +0200

    Add more methods to service to be detected as Java project in GitHub

--- src/main/java/com/example/fooCodeClimate/service/FooService.java
+++ src/main/java/com/example/fooCodeClimate/service/FooService.java
@@ -11,4 +11,14 @@ public interface FooService {
     public void printOut( int n );
 
     public <AnyType extends Comparable<? super AnyType>> int binarySearch( AnyType [ ] a, AnyType x );
+
+    public int [ ][ ] multiply( int [ ][ ] a, int [ ][ ] b );
+
+    public int fibonacci( int n );
+
+    public double eval( int n );
+
+    public double eval2( int n );
+
+    public void optMatrix( int [ ] c, long [ ][ ] m, int [ ][ ] lastChange );
 }
--- src/main/java/com/example/fooCodeClimate/service/FooServiceImpl.java
+++ src/main/java/com/example/fooCodeClimate/service/FooServiceImpl.java
@@ -55,4 +55,92 @@ public class FooServiceImpl implements FooService{
         }
         return -1;
     }
+
+    public int [ ][ ] multiply( int [ ][ ] a, int [ ][ ] b )
+    {
+        int n = a.length;
+        int [ ][ ] c = new int[ n ][ n ];
+
+        for( int i = 0; i < n; i++ )    // Initialization
+            for( int j = 0; j < n; j++ )
+                c[ i ][ j ] = 0;
+
+        for( int i = 0; i < n; i++ )
+            for( int j = 0; j < n; j++ )
+                for( int k = 0; k < n; k++ )
+                    c[ i ][ j ] += a[ i ][ k ] * b[ k ][ j ];
+
+        return c;
+    }
+
+    public int fibonacci( int n )
+    {
+        if( n <= 1 )
+            return 1;
+
+        int last = 1;
+        int nextToLast = 1;
+        int answer = 1;
+        for( int i = 2; i <= n; i++ )
+        {
+            answer = last + nextToLast;
+            nextToLast = last;
+            last = answer;
+        }
+        return answer;
+    }
+
+    public double eval( int n )
+    {
+        if( n == 0 )
+            return 1.0;
+        else
+        {
+            double sum = 0.0;
+            for( int i = 0; i < n; i++ )
+                sum += eval( i );
+            return 2.0 * sum / n + n;
+        }
+    }
+
+    public double eval2( int n )
+    {
+        double [ ] c = new double [ n + 1 ];
+
+        c[ 0 ] = 1.0;
+        for( int i = 1; i <= n; i++ )
+        {
+            double sum = 0.0;
+            for( int j = 0; j < i; j++ )
+                sum += c[ j ];
+            c[ i ] =  2.0 * sum / i + i;
+        }
+
+        return c[ n ];
+    }
+
+    public void optMatrix( int [ ] c, long [ ][ ] m, int [ ][ ] lastChange )
+    {
+        int n = c.length - 1;
+
+        for( int left = 1; left <= n; left++ )
+            m[ left ][ left ] = 0;
+        for( int k = 1; k < n; k++ )   // k is right - left
+            for( int left = 1; left <= n - k; left++ )
+            {
+                // For each position
+                int right = left + k;
+                m[ left ][ right ] = Integer.MAX_VALUE;
+                for( int i = left; i < right; i++ )
+                {
+                    long thisCost = m[ left ][  i ] + m[ i + 1 ][ right ]
+                            + c[ left - 1 ] * c[ i ] * c[ right ];
+                    if( thisCost < m[ left ][ right ] )  // Update min
+                    {
+                        m[ left ][ right ] = thisCost;
+                        lastChange[ left ][ right ] = i;
+                    }
+                }
+            }
+    }
 }
